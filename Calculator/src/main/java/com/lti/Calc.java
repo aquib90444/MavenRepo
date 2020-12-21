5b2644f7452f0e6a2e769a160ded7c7e29be66ff
aquib90444 <master.aquib786@gmail.com>
2020-12-21 15:01:13 +0530

    Added Sub function

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,8 +5,12 @@ public class Calc {
 	public static int add(int n1,int n2){
 		return n1+n2;
 	}
+	public static int sub(int n1,int n2){
+		return n1-n2;
+	}
 
 	public static void main(String[] args) {
-		System.out.println(add(10,20));
+		System.out.println("Sum= "+add(10,20));
+		System.out.println("Difference= "+sub(10,20));
 	}
 }
