de600ee65e11e571631f3c8a76fc10c719732331
aquib90444 <master.aquib786@gmail.com>
2020-12-21 15:09:38 +0530

    Added product function

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,9 +8,13 @@ public class Calc {
 	public static int sub(int n1,int n2){
 		return n1-n2;
 	}
+	public static int prod(int n1,int n2){
+		return n1*n2;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Sum= "+add(10,20));
-		System.out.println("Difference= "+sub(10,20));
+		System.out.println("Difference= "+sub(70,20));
+		System.out.println("Difference= "+prod(10,20));
 	}
 }
