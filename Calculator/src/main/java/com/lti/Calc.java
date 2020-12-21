5b80f05843202b836f2106afc326fe0b11ae4bc2
aquib90444 <master.aquib786@gmail.com>
2020-12-21 15:17:38 +0530

    Added div function

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -11,10 +11,14 @@ public class Calc {
 	public static int prod(int n1,int n2){
 		return n1*n2;
 	}
+	public static int div(int n1,int n2){
+		return n1/n2;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Sum= "+add(10,20));
 		System.out.println("Difference= "+sub(70,20));
 		System.out.println("Product= "+prod(10,20));
+		System.out.println("Division= "+div(42,3));
 	}
 }
